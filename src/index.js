3e978f18260cfdbca917465ff6e2bcf9ec8e6127
ShawnFrost26 <sb262202@gmail.com>
2024-03-15 17:01:05 +0530

    added cors and auth routes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 const movieRoutes = require("./routes/movieRoutes");
+const authRoutes = require("./routes/authRoutes");
+const cors = require("cors");
 
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(express.json());
 app.use("/api/movies", movieRoutes);
+app.use("/api/auth", authRoutes);
 
 mongoose
   .connect(MONGODB_URI)
